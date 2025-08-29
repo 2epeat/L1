32b8f2e1968757324149390c4b0ae236b8af2ad7
2epeat <nonint@ya.ru>
2025-08-29 05:56:11 +0300

    l1.7

--- L1.7/main.go
+++ L1.7/main.go
@@ -1,9 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
 /*Реализовать безопасную для конкуренции запись данных в структуру map.
 Подсказка: необходимость использования синхронизации (например, sync.Mutex или встроенная concurrent-map).
 Проверьте работу кода на гонки (util go run -race).*/
 
-func main() {
+var myMap = make(map[int]int)
+var wg sync.WaitGroup
+var mutex = &sync.Mutex{}
 
+func main() {
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			mutex.Lock()
+			myMap[i] = i
+			mutex.Unlock()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(myMap)
 }
