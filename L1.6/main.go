a5d5e5f3fc616dfa6ac7ea91bbae2da290a1cfa2
2epeat <nonint@ya.ru>
2025-08-29 05:07:08 +0300

    l1.6

---
+++ L1.6/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"os/signal"
+	"runtime"
+	"sync"
+	"syscall"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	// ---
+	go func() {
+		fmt.Println("native stopping")
+		return
+	}()
+
+	// ---
+	go func() {
+		fmt.Println("stopped by Goexit")
+		runtime.Goexit()
+	}()
+
+	// ---
+	go func() {
+		for {
+			if rand.Intn(100) > 50 {
+			} else {
+				fmt.Println("stopped by condition")
+				return
+			}
+		}
+	}()
+
+	// ---
+	timeout := time.After(10 * time.Millisecond)
+	go func() {
+		for {
+			select {
+			case <-timeout:
+				fmt.Println("stopped by After timeout")
+				return
+			}
+		}
+	}()
+
+	// ---
+	go func() {
+		shutdown, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+
+		for {
+			select {
+			case <-shutdown.Done():
+				fmt.Println("stopped by context with timeout")
+				return
+			default:
+			}
+		}
+	}()
+
+	// ---
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case <-done:
+				fmt.Println("stopped by channel")
+				return
+			default:
+			}
+		}
+	}()
+	time.Sleep(1 * time.Second)
+	done <- true
+
+	// ---
+	go func() {
+		for {
+			select {
+			case v, ok := <-done:
+				if !ok {
+					fmt.Println("stopped by closing channel")
+					return
+				}
+				done <- v
+			}
+		}
+	}()
+	time.Sleep(1 * time.Second)
+	close(done)
+
+	// ---
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+		defer stop()
+
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("stopped by context with signal")
+				return
+			default:
+			}
+		}
+	}()
+
+	wg.Wait()
+}
