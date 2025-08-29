6ac9161a669c802f703581c140c4db88f0efc5c4
2epeat <nonint@ya.ru>
2025-08-29 20:28:33 +0300

    l1.12

--- L1.12/main.go
+++ L1.12/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+/*Имеется последовательность строк: ("cat", "cat", "dog", "cat", "tree").
+Создать для неё собственное множество.
+Ожидается: получить набор уникальных слов.
+Для примера, множество = {"cat", "dog", "tree"}.*/
+
+var arr = []string{"cat", "cat", "dog", "cat", "tree"}
+
+var m = make(map[string]bool)
+var s = []string{}
+
+func main() {
+	for _, v := range arr {
+		if _, ok := m[v]; !ok {
+			m[v] = true
+			s = append(s, v)
+		}
+	}
+	fmt.Println(s)
 }
